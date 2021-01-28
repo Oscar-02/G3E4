aaf4d9840efdd929e3c38525d2ebc75bf6ef22af
Oscar-02 <oscacanas29@gmail.com>
2021-01-28 16:34:32 -0600

    0.1 Start Project

---
+++ G3E4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace G3E4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
